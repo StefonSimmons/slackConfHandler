6b03a3fe3b7dcc86bfee783a0e261c08ed96bd10
StefonSimmons <stefon.simmons@datadoghq.com>
2022-11-18 17:40:45 -0500

    getting channels convos by date

--- app.js
+++ app.js
@@ -2,57 +2,15 @@ const { WebClient } = require('@slack/web-api');
 
 const web = new WebClient(process.env.SLACK_CONF_BOT_TOKEN);
 
-const currentTime = new Date().toTimeString();
-
-// async function postMessage () {
-
-//     try {
-//       // Use the `chat.postMessage` method to send a message from this app
-//       await web.chat.postMessage({
-//         channel: '#general',
-//         text: `The current time is ${currentTime}, Dog :dog`,
-//       });
-//       console.log('Message posted!');
-//     } catch (error) {
-//       console.log(error);
-//     }
-  
-// }
-
-// postMessage()
-
-async function getChannels () {
-
+const getChannelHistory = async (channelID, oldestTime, latestTime) => {
+    // console.log(channelID, oldestTime, latestTime)
     try {
-      const list = await web.conversations.list({types:'public_channel,private_channel,mpim,im'});
-      console.log('List of channels, and direct messages!');
+      const list = await web.conversations.history({channel:channelID, oldest: oldestTime, latest: latestTime});
+      console.log('Message Events');
       console.log(list)
     } catch (error) {
-      console.log(error);
+      console.log(error.data);
     }
-  
 }
 
-getChannels()
-
-// async function getChannelHistory() {
-
-//     try {
-//       const list = await web.conversations.history({channel:'C04B60VT3TR'});
-//       console.log('Convo or Message Events');
-//       console.log(list)
-//     } catch (error) {
-//       console.log(error.data);
-//     }
-  
-// }
-
-// getChannelHistory()
-
-
-
-// generally:
-// I  want to send a request to an api (the one I created) that recieves information on my 
-// get the slack Conversation ID of the Conversation the command was called in.
-// get the slack conversation from start to end date/time
-// 
+module.exports = {getChannelHistory}
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const logger = require('morgan')
+const {getChannelHistory} = require('./app')
+
 
 const PORT = process.env.PORT || 3000
 app.use(logger('combined'))
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 })
@@ -13,7 +16,49 @@ app.get('/', (_, res) => {
     res.json({"greeting": "welcome", "today": new Date()})
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
+app.post('/', async (req, res) => {
+    /**
+       {
+         token: '4HgTB5nDVTEUDHvhUw34NbGV',
+         team_id: 'T04BHJB9B7F',
+         team_domain: 'testslacktoco-rzi8358',
+         channel_id: 'C04B60VT3TR',
+         channel_name: 'test-channel',
+         user_id: 'U04BZ64JAUR',
+         user_name: 'stefonsimmons1',
+         command: '/save-message',
+         text: '11/21/2022 12:11:09; 11/21/2022 13:08:15',
+         api_app_id: 'A04BLHB7MPC',
+         is_enterprise_install: 'false',
+         response_url: 'https://hooks.slack.com/commands/T04BHJB9B7F/4420002667344/JhX1bZYCkN5Uh2IguRNn90T7',
+         trigger_id: '4408942534097.4391623317253.98ffa22b9781055b4495a983b7be6e8c'
+       }
+     */
+  
+    const {body}= req
+
+    const dateArr = body.text.split('; ')
+    const oldest = dateArr[0]
+    const oldestDateStr = oldest.split(' ')[0]
+    const oldestTimeStr = oldest.split(' ')[1]
+
+    const latest = dateArr[1]
+    const latestDateStr = latest.split(' ')[0]
+    const latestTimeStr = latest.split(' ')[1]
+    const oldestDateArr = oldestDateStr.split('/')
+    const latestDateArr = latestDateStr.split('/')
+
+    const oldestTimeArr = oldestTimeStr.split(':')
+    const latestTimeArr = latestTimeStr.split(':')
+
+    const oldestDate = new Date(oldestDateArr[2], oldestDateArr[0]-1, oldestDateArr[1], oldestTimeArr[0], oldestTimeArr[1], oldestTimeArr[2])
+    const latestDate = new Date(latestDateArr[2], latestDateArr[0]-1, latestDateArr[1], latestTimeArr[0], latestTimeArr[1], latestTimeArr[2])
+    const channelID = body.channel_id
+    const oldestMS = ((oldestDate.getTime()/1000)-1).toString()
+    const latestMS = ((latestDate.getTime()/1000)+1).toString()
+
+    await getChannelHistory(channelID, oldestMS, latestMS)
+
+    
     res.json(req.body)
 })
